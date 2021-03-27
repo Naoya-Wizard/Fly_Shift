45f4c91b7a7e1edc9d3ccefa8137c66382c149ce
naoya-fuke <79356057+naoya-fuke@users.noreply.github.com>
2021-03-27 11:02:09 +0900

    newのルートとnew.html.erbを作成

--- app/controllers/creates_controller.rb
+++ app/controllers/creates_controller.rb
@@ -1,2 +1,6 @@
 class CreatesController < ApplicationController
+    def index
+    end
+    def new
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: 'creates#index'
+  resources :creates, only: [:new]
 end
